9fa6119606c4cfa2d8a7c81ad301885ee979ea60
hirokam <h.kameyama0329@gmail.com>
2024-03-26 01:02:08 +0900

    add mysql

--- src/resources/views/storage.blade.php
+++ src/resources/views/storage.blade.php
@@ -1,24 +1,35 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Storage確認</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/storage.css') }}">
 </head>
+
+<style>
+    body {
+        background: #dcdcdc;
+        font-size: 15px;
+    }
+</style>
+
 <body>
-    <div class="storage-form">
+    <div class="storage-form__rename-group">
         <form action="/storage" method="post" enctype="multipart/form-data">
         @csrf
-            <label>リネームされて保存：<input type="file" name="image"></label>
-            <button>アップロード</button>
+            <label class="rename">リネームされて保存<span class="colon">：</span><input type="file" name="image" class="input"></label>
+            <button class="upload__button">アップロード</button>
         </form>
     </div>
 
-    <div class="storage-form">
+    <div class="storage-form__named-group">
         <form action="/storage/named" method="post" enctype="multipart/form-data">
         @csrf
-            <label>アップロードした時の名前で保存：<input type="file" name="image"></label>
-            <button>アップロード</button>
+            <label class="named">アップロードした時の名前で保存：<input type="file" name="image" class="input"></label>
+            <button class="upload__button">アップロード</button>
         </form>
     </div>
 </body>
