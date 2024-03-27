d48a00c4daa68b11db2ebbdf4f36318f45378d15
hirokam <h.kameyama0329@gmail.com>
2024-03-27 11:51:19 +0900

    add header

--- src/resources/views/storage_test.blade.php
+++ src/resources/views/storage_test.blade.php
@@ -12,51 +12,59 @@
 <style>
     body {
         background: #dcdcdc;
-        margin: 100px 50px;
+    }
+
+    main {
+        margin: 80px 50px;
         font-size: 15px;
     }
 </style>
 
 <body>
-    <div class="content-all">
-        <div class="frame">
-            <div class="storage-form__to-sample-group">
-                <form action="/storage/rename/to_sample" method="post" enctype="multipart/form-data">
-                @csrf
-                    <label class="rename">リネームされてsampleに保存
-                        <div class="input"><input type="file" name="image" class="input"></div>
-                    </label>
-                    <button class="upload__button">アップロード</button>
-                </form>
+    <header>
+        <h1>Storage保存と表示サンプル</h1>
+    </header>
+    <main>
+        <div class="content-all">
+            <div class="frame">
+                <div class="storage-form__to-sample-group">
+                    <form action="/storage/rename/to_sample" method="post" enctype="multipart/form-data">
+                    @csrf
+                        <label class="rename">リネームされてsampleに保存
+                            <div class="input"><input type="file" name="image" class="input"></div>
+                        </label>
+                        <button class="upload__button">アップロード</button>
+                    </form>
+                </div>
+            </div>
+            <div class="frame">
+                <div class="storage-form__to-sample-group">
+                    <form action="/storage/named/to_named_sample" method="post" enctype="multipart/form-data">
+                    @csrf
+                        <label class="named">アップロードした時の名前でnamed_sampleに保存
+                            <div class="input"><input type="file" name="image" class="input"></label></div>
+                        <button class="upload__button">アップロード</button>
+                    </form>
+                </div>
+            </div>
+            <div class="frame">
+                <div class="storage-form__to-public-group">
+                    <form action="/storage/named/to_public" method="post" enctype="multipart/form-data">
+                    @csrf
+                        <label class="named">アップロードした時の名前でpublicに保存
+                            <div class="input"><input type="file" name="image" class="input"></label></div>
+                        <button class="upload__button">アップロード</button>
+                    </form>
+                </div>
             </div>
         </div>
-        <div class="frame">
-            <div class="storage-form__to-sample-group">
-                <form action="/storage/named/to_named_sample" method="post" enctype="multipart/form-data">
-                @csrf
-                    <label class="named">アップロードした時の名前でnamed_sampleに保存
-                        <div class="input"><input type="file" name="image" class="input"></label></div>
-                    <button class="upload__button">アップロード</button>
-                </form>
+        <div class="show-images">
+            @foreach($images as $image)
+            <div class="show-image">
+                <img src="{{ asset($image->path) }}" class="picture">
             </div>
+            @endforeach
         </div>
-        <div class="frame">
-            <div class="storage-form__to-public-group">
-                <form action="/storage/named/to_public" method="post" enctype="multipart/form-data">
-                @csrf
-                    <label class="named">アップロードした時の名前でpublicに保存
-                        <div class="input"><input type="file" name="image" class="input"></label></div>
-                    <button class="upload__button">アップロード</button>
-                </form>
-            </div>
-        </div>
-    </div>
-    <div class="show-images">
-        @foreach($images as $image)
-        <div class="show-image">
-            <img src="{{ asset($image->path) }}" class="picture">
-        </div>
-        @endforeach
-    </div>
+    </main>
 </body>
 </html>
